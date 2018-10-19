5554fe79471f0ec052fb85c54970087936f5d96c
daniel95743 <daniellanding96@gmail.com>
2018-10-19 16:55:31 +0800

    Add Ch3

---
+++ Ch3_Lab1/source/main.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Odd(int U);
+int Even(int U);
+int TotalSum(int U);
+
+int main(void)
+{
+	int n, Sum;
+	char AddChoice;
+	printf("1+2+...+n=?請輸入n=");
+	scanf_s("%d",&n);
+	getchar();
+	printf("請問要奇數和(o),偶數(e),還是整數和(i)?請選擇：");
+	scanf_s("%c", &AddChoice);
+
+	switch (AddChoice)
+	{
+	case'o':
+		Sum = Odd(n);
+		break;
+
+	case'e':
+		Sum = Even(n);
+		break;
+
+	case'i':
+		Sum = TotalSum(n);
+		break;
+	default:
+		printf("選擇錯誤\n");
+		return -1;
+	}
+
+	printf("總和為%d\n", Sum);
+	system("pause");
+	return 0;
+}
+int Odd(int U)
+{
+	int i, total = 0;
+	for (i = 1; i <= U; i++)
+		if (i % 2 == 1)
+			total = total + i;
+	return total;
+}
+
+int Even(int U)
+{
+	int i, total = 0;
+	for (i = 1; i <= U; i++)
+		if (i % 2 == 0)
+			total = total + i;
+	return total;
+}
+int TotalSum(int U)
+{
+	return Odd(U)+Even(U);
+}
